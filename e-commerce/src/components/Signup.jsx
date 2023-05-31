e073065b2d4552521ca0cfde8e557803684b9c5b
kareem magdy <kareemmagdy85888@gmail.com>
2023-05-31 19:40:34 +0300

    login and sign components

---
+++ e-commerce/src/components/Signup.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useState } from 'react'
+export function Signup() {
+    const [email,setemail]=useState("");
+    const [password,setpassword]=useState("");
+    const [name,setname]=useState("");
+
+    const handlesubmit = (e) => {
+        e.preventDefault();
+        console.log(email)
+        console.log(password)
+        setpassword("");
+        setemail("");
+    };
+
+  return (
+    <div className='body'>
+        <div className='auth-form-container'>
+              <form className='sign' action="" onSubmit={handlesubmit}>
+                <label htmlFor="email"></label>
+                <input type="email" id='email' name='email' placeholder='Email'   value={email} onChange={(e) => setemail(e.target.value)}/>
+
+                <label htmlFor="password"></label>
+                <input type="password" id='password' name='password'  value={password} onChange={(e) => setpassword(e.target.value)} placeholder='Password'/>
+                
+                <label htmlFor="name"></label>
+                <input type="text" id='name' name='name'  value={name} onChange={(e) => setname(e.target.value)} placeholder='Name'/>
+                
+                <button  type='submit' className='bg-success sub'>Sign UP</button>
+                <button className='link-btn' >already have an account? Log In</button>
+            </form>
+
+            
+        
+
+        </div>
+    </div>
+  )
+}
