754da09fd77c6cb791c4ae133b21c705e2e0fa3a
“hashem_muhammed” <hashemuhammed@gmail.com>
2023-06-06 00:22:20 +0300

    fixed bug

--- e-commerce/src/components/NotAuthorized.jsx
+++ e-commerce/src/components/NotAuthorized.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
 
 export default function NotAuthorized() {
   return (
-    <div>NotAuthorized</div>
-  )
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card mt-5">
+            <div className="card-body text-center">
+              <h1 className="card-title mb-4">Access Denied</h1>
+              <p className="card-text mb-4">You are not authorized to access this page.</p>
+              <Link to="/products" className="btn btn-primary mx-2">Go to Home</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
