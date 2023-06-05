eda7e925e27883eda5b97b50bb922ec5ee283d18
kareem magdy <kareemmagdy85888@gmail.com>
2023-06-06 00:44:18 +0300

    connecting every thing together

--- e-commerce/src/Components/MyNav.jsx
+++ e-commerce/src/Components/MyNav.jsx
@@ -15,7 +15,7 @@ let navigate = useNavigate()
   let logout = () =>{
     dispatch(log_out());
     localStorage.removeItem("token");
-    navigate("/login");
+    navigate("/");
   } 
   return (
      <div>  
@@ -23,12 +23,12 @@ let navigate = useNavigate()
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#Products">Products</Nav.Link>
-          {currentUser&&currentUser.role && <Nav.Link>Admin panel</Nav.Link>}
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/products">Products</Nav.Link>
+          {currentUser&&currentUser.role && <Nav.Link href="dminpanel">Admin panel</Nav.Link>}
         </Nav>
         <Nav className="mx-5">
-          {!currentUser&& <Nav.Link >Login</Nav.Link>}
+          {!currentUser&& <Nav.Link  href="/login">Login</Nav.Link>}
           {currentUser&& <Nav.Link className="text-capitalize fs-5"> {currentUser.name} </Nav.Link>}
           {currentUser&& <Nav.Link className="fs-5"> <BsFillCartCheckFill /> </Nav.Link>}
           {currentUser&& <Nav.Link className="btn fs-5" onClick={logout}>Sign out</Nav.Link>}
--- e-commerce/src/pages/Home.jsx
+++ e-commerce/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { AllPlanets } from "../components/AllPlanets";
 import React from 'react'
 import { Product } from "../components/Product";
+import { Slider } from "../components/Slider";
 
 export  function Home() {
   return (
     <div>
+        <Slider />
         < Product/>
     </div>
   )
